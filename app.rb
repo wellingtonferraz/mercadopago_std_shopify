fda5d1cffb1331f0e10820e60ee7f7c7c9d9f0d7
Joel Ibaceta <niohnex@gmail.com>
2016-06-08 16:34:50 -0300

    ipn finished

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'json'
 require 'addressable/uri'
 require 'mercadopago' 
 require 'pp'
-
+require 'digest/hmac'
 
 class MercadoPagoStd < Sinatra::Base
 
@@ -21,8 +21,8 @@ class MercadoPagoStd < Sinatra::Base
   INTERNAL_BASE = "https://internal.mercadolibre.com"
 
   def initialize(base_path: '')
-    @base_path = base_path
-    @key = 'GEO8BxYT5ifSOp9Y3ys7wGiwXcNu8h0O'
+    @base_path  = base_path
+    @key        = 'GEO8BxYT5ifSOp9Y3ys7wGiwXcNu8h0O'
     super
   end
 
@@ -36,27 +36,32 @@ class MercadoPagoStd < Sinatra::Base
   
   
   def get_owner_id(client_id)
-    owner = HTTParty.get("#{API_BASE}/applications/#{client_id.to_s}") 
+    owner = HTTParty.get("#{API_BASE}/applications/#{client_id.to_s}")  
+    
     owner.code.to_i === 200 ? owner['owner_id'] : nil
   end
 
   def get_secret_key(owner_id, client_id)
     query               = "caller.id=#{owner_id.to_s}&caller.status=ACTIVE&caller.scopes=crud_app"
-    url_internal        = "#{INTERNAL_BASE}applications/#{client_id.to_s}?#{query}"
+    url_internal        = "#{INTERNAL_BASE}/applications/#{client_id.to_s}?#{query}"
     response_secret_key = HTTParty.get(url_internal) 
     return response_secret_key.code.to_i === 200  ? response_secret_key['secret_key'] : nil
   end
   
   
-  get '/' do; "Mercado Pago Teste STD Checkout"; end
+  get '/' do
+    "Mercado Pago Teste STD Checkout"
+  end
 
   post '/' do
     
-    client_id     = fields['x_account_id']
-    client_secret = get_secret_key(get_owner_id(client_id), client_id)
+    #client_id     = fields['x_account_id']
+    #client_secret = get_secret_key(get_owner_id(client_id), client_id)
     
-    MercadoPago::Settings.CLIENT_ID     = client_id
-    MercadoPago::Settings.CLIENT_SECRET = client_secret
+    MercadoPago::Settings.CLIENT_ID     = '6961738956989181'#client_id
+    MercadoPago::Settings.CLIENT_SECRET = 'vPrAA7HX3zFFkhhUEv7LXHOcVqbeSjbH'#client_secret
+    
+    MercadoPago::Preference.set_custom_header("X-Tracking-Id", "platform:desktop,type:shopify,so:1.0")
     
     preference = MercadoPago::Preference.new
     preference.external_reference = fields['x_reference']
@@ -97,9 +102,13 @@ class MercadoPagoStd < Sinatra::Base
 
   get '/callback' do 
     
-
-    MercadoPago::Preference.load(params[:preference_id]) do |preference|
-      preference = preference
+    MercadoPago::Settings.CLIENT_ID     = '6961738956989181'#client_id
+    MercadoPago::Settings.CLIENT_SECRET = 'vPrAA7HX3zFFkhhUEv7LXHOcVqbeSjbH'#client_secret
+    
+    preference = nil
+    
+    MercadoPago::Preference.load(params[:preference_id]) do |p|
+      preference = p
     end 
     
     MercadoPago::Payment.load(params[:collection_id]) do |collection|
@@ -109,49 +118,69 @@ class MercadoPagoStd < Sinatra::Base
     
     
     additional_info = eval(preference.additional_info)
+    
+    p "ADDITIONAL INFO"
+    
+    pp eval(preference.additional_info)
 
     ts = Time.now.utc.iso8601
 
     result = {timestamp: ts}
      
     payload = {
-      'x_account_id'        => additional_info['x_account_id'],
-      'x_reference'         => additional_info['x_reference'],
-      'x_currency'          => additional_info['x_currency'],
-      'x_test'              => additional_info['x_test'],
-      'x_amount'            => additional_info['x_amount'],
+      'x_account_id'        => additional_info[:x_account_id],
+      'x_reference'         => additional_info[:x_reference],
+      'x_currency'          => additional_info[:x_currency],
+      'x_test'              => additional_info[:x_test],
+      'x_amount'            => additional_info[:x_amount],
       'x_result'            => (ACTION[params[:collection_status]] || 'failed'),
       'x_gateway_reference' => SecureRandom.hex,
       'x_timestamp'         => ts
     }
 
-    payload[:x_signature] = sign(payload)
+    payload[:x_signature] = sign(payload, MercadoPago::Settings.CLIENT_SECRET)
+    
+    p "PAYLOAD"
+    pp payload
 
     redirect_url = Addressable::URI.parse(additional_info[:x_url_complete])
     redirect_url.query_values = payload 
     
-    case params[:collection_status]
-    when 'approved' || 'rejected'
-      response = HTTParty.post(additional_info[:x_url_complete], body: payload)
-      redirect redirect_url     if response.code == 200
-      result[:error] = response unless response.code == 200 
-    when 'pending' || 'in_process'
-      # Do
+    p "COLLECTION STATUS : #{params[:collection_status]}"
+    status = params[:collection_status].to_s
+    
+    case status
+      when 'approved', 'rejected'
+    
+        response = HTTParty.post(additional_info[:x_url_complete], body: payload)
+    
+        p "RESPONSE"
+    
+        pp response
+    
+        redirect redirect_url     if response.code      == 200
+        result[:error] = response unless response.code  == 200 
+        
+      when 'in_process', 'pending'
+        p "REDIRECTING"
+        redirect redirect_url
     end
 
 
   end
   
-  get '/ipn' do
+  post '/ipn' do
      
-    client_id     = params[:x_account_id]
-    client_secret = get_secret_key(get_owner_id(client_id), client_id)
+    #client_id     = params[:x_account_id]
+    #client_secret = get_secret_key(get_owner_id(client_id), client_id)
     
-    MercadoPago::Settings.CLIENT_ID     = client_id
-    MercadoPago::Settings.CLIENT_SECRET = client_secret
-    
+    MercadoPago::Settings.CLIENT_ID     = '6961738956989181'#client_id
+    MercadoPago::Settings.CLIENT_SECRET = 'vPrAA7HX3zFFkhhUEv7LXHOcVqbeSjbH'#client_secret
+        
     path, query   = env['REQUEST_PATH'], env['QUERY_STRING'] 
-    params = query.split('&').map{|q| {q.split('=')[0].to_sym => q.split('=')[1]}}.reduce Hash.new, :merge
+    pp  query
+    
+    params = query.split('&').map{|q| {q.split('=')[0].to_s.to_sym => q.split('=')[1]}}.reduce Hash.new, :merge
     
     notification = MercadoPago::Notification.new(params)  
     
@@ -174,36 +203,43 @@ class MercadoPagoStd < Sinatra::Base
         
       paid = merchant_order.payments.map{ |p| p.status == 'approved' ? p.transaction_amount : 0}.reduce(:+) 
       
-      MercadoPago::MerchantOrder.load(payment.collection["merchant_order_id"]) do |merchant_order|
+      
+      
+      
+        pp merchant_order
         
         if paid >= merchant_order.total_amount # If a payments is completed 
           MercadoPago::Preference.load(merchant_order.preference_id) do |preference|
-          
+        
             additional_info = eval(preference.additional_info)
             ts              = Time.now.utc.iso8601
-         
+          
+            pp additional_info
+          
             payload = {
-              'x_account_id'        => additional_info['x_account_id'],
-              'x_reference'         => additional_info['x_reference'],
-              'x_currency'          => additional_info['x_currency'],
-              'x_test'              => additional_info['x_test'],
-              'x_amount'            => additional_info['x_amount'],
-              'x_result'            => (ACTION[params[:collection_status]] || 'failed'),
+              'x_account_id'        => additional_info[:x_account_id],
+              'x_reference'         => additional_info[:x_reference],
+              'x_currency'          => additional_info[:x_currency],
+              'x_test'              => additional_info[:x_test],
+              'x_amount'            => additional_info[:x_amount],
+              'x_result'            => 'completed',
               'x_gateway_reference' => SecureRandom.hex,
               'x_timestamp'         => ts
             }
-        
+      
             redirect_url              = Addressable::URI.parse(additional_info[:x_url_complete])
             redirect_url.query_values = payload 
-         
+          
+            HTTParty.get(additional_info[:x_url_complete], body: payload)
+       
             response = HTTParty.post(additional_info[:x_url_complete], body: payload)
-        
-            redirect redirect_url     if response.code      == 200
-            result[:error] = response unless response.code  == 200 
+          
+          
+      
+          
           end
         end
-      end 
-         
+   
     rescue
       # if the merchant order doesnt exist
     end
